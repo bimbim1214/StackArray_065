36cd406f67b525d26bdb533b5104dc52b0d2e0e8
bimbim1214 <bimoadityapangestu@gmail.com>
2023-06-16 15:12:03 +0700

    dan membuat for untuk perulanganint tmp sama dengan topo dan tmp nnya lebih besar sama dengan 0

--- stackusingarray/stackusingarray.cpp
+++ stackusingarray/stackusingarray.cpp
@@ -47,6 +47,10 @@ public:
 		if (empty()) {
 			cout << "\nstack is empty." << endl;
 		}
-		
+		else {
+			for (int tmp = top; tmp >= 0; tmp--) {
+				cout << stack_array[tmp] << endl;
+			}
+		}
 	}
 };
